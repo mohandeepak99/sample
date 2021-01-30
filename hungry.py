9c59e3f3da47a7ac205b51504b39fc94d017227a
mohan deepak <mohandeepakyadav99@gmail.com>
2021-01-30 19:03:00 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,4 +2,6 @@ hungry=input("are you hungry")
 if hungry=="yes":
     print("eat chips")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty")
+    if thirsty=='yes':
+        print("drink water")
